e30ea5fdf2c69ce5ab313951338f7f6d5fddc732
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-14 16:05:51 +0300

    Добавлены задания блока 2

--- Task2/Subtask1/Program.cs
+++ Task2/Subtask1/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Programm{
+
+    class Human{
+        public string name;
+        public int age;
+
+        public void Greetings(){
+            Console.WriteLine("Привет, меня зовут {0}", name);
+        }
+
+            // Конструктор 1
+	    public Human()
+	    {
+	    	name = "Неизвестно";
+	    	age = 20;
+	    }
+	    // Конструктор 2
+	    public Human(string n)
+	    {
+	    	name = n;
+	    	age = 20;
+	    }
+	    // Конструктор 3
+	    public Human(string n, int a)
+	    {
+	    	name = n;
+	    	age = a;
+	    }
+    }
+
+    struct Animal{
+        public string name;
+        public string type;
+        public int age;
+
+        public void Greetings(){
+            Console.WriteLine("Привет, я {0}. Меня зовут {1}", type, name);
+        }
+    }
+    public static void Main(){
+        Animal myPet;
+        myPet.age = 2;
+        myPet.name = "Варя";
+        myPet.type = "Крыса";
+        myPet.Greetings();
+
+        Human me = new Human("Владимир", 27);
+        me.Greetings();
+        
+        Console.ReadKey();
+    }
+}
---
+++ Task2/Subtask2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Programm{
+
+    class Pen{
+        public string color;
+        public int cost;
+
+        // Конструктор 1
+	    public Pen()
+	    {
+            color = "Черный";
+            cost = 100;	    
+        }
+	    // Конструктор 2
+	    public Pen(string penColor, int penCost)
+	    {
+            color = penColor;
+            cost = penCost;	
+	    }
+    }
+
+    public static void Main(){
+        Pen pen1 = new Pen();
+        Pen pen2 = new Pen("Синий", 150);
+        Console.ReadKey();
+    }
+}
