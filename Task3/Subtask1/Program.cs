283c6ee4f478fbc4a588252f9836f6bf7ad20a04
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-18 00:01:29 +0300

    Добавлены задания блока 3

---
+++ Task3/Subtask1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+class Programm{
+
+    class Company{
+        public string Type;
+        public string Name;
+    }
+
+    class Departament {
+        public Company Company;
+        public City City;
+
+        public Departament(){
+            City = new City();
+            City.Name = "Санкт-Петербург";
+
+            Company = new Company();
+            Company.Type = "Банк";
+            Company.Name = "Адидас";
+        }
+
+            public Departament(string newCity){
+            City = new City();
+            City.Name = newCity;
+
+            Company = new Company();
+            Company.Type = "Банк";
+            Company.Name = "Адидас";
+            }
+        
+            public Departament(string newCity, string newCompany){
+            City = new City();
+            City.Name = newCity;
+
+            Company = new Company();
+            Company.Type = "Банк";
+            Company.Name = newCompany;
+            }
+            
+            public Departament(string newCity, string newCompany, string newType){
+            City = new City();
+            City.Name = newCity;
+
+            Company = new Company();
+            Company.Type = newType;
+            Company.Name = newCompany;
+            }
+            
+
+    }
+
+    class City{
+        public string Name;
+    }
+
+    public static void Main(){
+        var departament1 = GetCurrentDepartament();
+        var departament2 = GetCurrentDepartament("Пенза");
+        var departament3 = GetCurrentDepartament("Санкт-Петербург", null, "Банк");
+        var departament4 = GetCurrentDepartament("Санкт-Петербург", "Puma", "Банк");
+
+        CheckDepartament(departament1);
+        CheckDepartament(departament2);
+        CheckDepartament(departament3);
+        CheckDepartament(departament4);
+
+        Console.ReadKey();
+    }
+    static Departament GetCurrentDepartament(string newCity = "Санкт-Петербург", string newCompany = "Адидас", string newType = "Банк"){
+        Departament result = new Departament(newCity, newCompany, newType);
+        return result;
+    }
+
+    static void CheckDepartament(Departament departament) {
+        if (departament?.City?.Name == "Санкт-Петербург"&& departament?.Company?.Type == "Банк"){
+            Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", departament?.Company?.Name ?? "Неизвестная компания");
+        }
+    }
+
+}
---
+++ Task3/Subtask2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Programm{
+    class Bus
+    {
+    	public int? Load;
+
+    	public void PrintStatus()
+    	{
+            if (Load.HasValue){
+                Console.WriteLine("В автобусе {0} пассажиров", Load);
+            }
+            else{
+                Console.WriteLine("В автобусе пусто");
+            }
+    	}
+        public Bus(int load){
+            Load = load;
+        }
+        public Bus(){
+        }
+    }
+
+    static void Main(){
+        Bus bus1 = new Bus();
+        Bus bus2 = new Bus(143);
+        bus1.PrintStatus();
+        bus2.PrintStatus();
+        Console.ReadKey();
+    }
+}
