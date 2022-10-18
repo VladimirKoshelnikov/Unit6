500378cc8dae75c9fc2b30e9a0258272401d7234
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-10-18 14:38:07 +0300

    Добавлены задания блока 5

---
+++ Task5/Subtask1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Programm{
+    class Car{
+        public double fuel;
+        public int millage;
+
+        public Car(){
+            fuel = 50;
+            millage = 0;
+        }
+        
+        public void Move(){
+            fuel -= 0.5;
+            millage++;    
+        }
+
+        public void FillTheCar(){
+            fuel = 50;
+        }
+
+    }
+    static void Main(){
+        Car myCar = new Car();
+
+        while (myCar.fuel > 0){
+            myCar.Move();
+        }
+        Console.WriteLine("Вы проехали {0}км. Теперь у вас пустой бак и вы не можете ехать, но зато как роскошно вы идете с канистрой", myCar.millage);
+        Console.ReadKey();
+    }
+}
---
+++ Task5/Subtask2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Programm{
+
+    class Triangle{
+        public double a, b, c;
+        public double GetSquare(){}
+        public double GetPerimeter(){}
+    }
+
+    class Square{
+        public double side;
+        public double GetSquare(){}
+        public double GetPerimeter(){}
+    }
+    class Circle{
+        public double radius;
+        public double GetSquare(){}
+        public double GetPerimeter(){}
+    }
+    static void Main(){
+
+    }
+}
